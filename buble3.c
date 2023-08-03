0c9ffb97b87ceb10a2e6fb83939f1c3fed7069be
nahid0-0 <124519873+nahid0-0@users.noreply.github.com>
2023-08-03 14:02:01 +0600

    Create buble3.c

---
+++ buble3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void buble(int n, int a[]){
+    for(int i = 0; i < n; ++i){
+        int dit = 0;
+        for(int j = 0; j < n - 1; ++j){
+            if(a[j] < a[j+1]){
+                if(a[j] > a[j+1]){
+                    int temp = a[j+1];
+                    a[j+1] = a[j];
+                    a[j] = temp;
+                }
+                dit = 1;
+            }
+        }
+        if(!dit) break;
+    }
+}
+int main(){
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for(int i = 0; i < n; ++i){
+        scanf("%d", &a[i]);
+    }
+    buble(n, a);
+    for(int i = 0; i < n; ++i){
+        printf("%d \n", a[i]);
+    }
+    return 0;
+}
